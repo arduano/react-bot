8ad6d8159b6cc2396afa61b56c638b3f83298bd0
arduano <leonid.shchurov@gmail.com>
2022-01-26 20:59:48 +1100

    Ported to Rust

---
+++ src/config.rs
@@ -0,0 +1,90 @@
+use std::{collections::HashMap, path::PathBuf, str::FromStr};
+
+use indexmap::IndexMap;
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
+use serde_json::Map;
+use twilight_model::{
+    channel::ReactionType,
+    id::{
+        marker::{ChannelMarker, MessageMarker, RoleMarker},
+        ChannelId, Id, MessageId,
+    },
+};
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(untagged)]
+pub enum VecOrSingle {
+    Single(Id<RoleMarker>),
+    Vec(Vec<Id<RoleMarker>>),
+}
+
+pub enum VecOrSingleIter<'a> {
+    Single(std::iter::Once<Id<RoleMarker>>),
+    Vec(std::slice::Iter<'a, Id<RoleMarker>>),
+}
+
+impl VecOrSingle {
+    pub fn iter<'a>(&'a self) -> VecOrSingleIter<'a> {
+        match self {
+            Self::Single(id) => VecOrSingleIter::Single(std::iter::once(*id)),
+            Self::Vec(ids) => VecOrSingleIter::Vec(ids.iter()),
+        }
+    }
+}
+
+impl<'a> Iterator for VecOrSingleIter<'a> {
+    type Item = Id<RoleMarker>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self {
+            Self::Single(iter) => iter.next(),
+            Self::Vec(iter) => iter.next().cloned(),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Config {
+    pub channel: Id<ChannelMarker>,
+    pub message: Id<MessageMarker>,
+
+    #[serde(rename = "reactMap")]
+    // Using IndexMap to preserve the order
+    pub react_map: IndexMap<String, VecOrSingle>,
+}
+
+impl Config {
+    pub fn get_roles_for_react(&self, react: &ReactionType) -> Option<&VecOrSingle> {
+        match react {
+            ReactionType::Unicode { name } => self.react_map.get(name),
+            ReactionType::Custom { id, .. } => self.react_map.get(&id.to_string()),
+        }
+    }
+}
+
+pub fn read_config() -> Vec<Config> {
+    let config_path = PathBuf::from_str("./config.json").unwrap();
+    let config_file = std::fs::read_to_string(config_path).unwrap();
+    let config: Vec<Config> = serde_json::from_str(&config_file).unwrap();
+    config
+}
+
+pub fn get_react_roles<'a>(
+    configs: &'a Vec<Config>,
+    message_id: Id<MessageMarker>,
+    channel_id: Id<ChannelMarker>,
+    emoji: &ReactionType,
+) -> Option<&'a VecOrSingle> {
+    let config = configs
+        .iter()
+        .find(|conf| conf.message == message_id && conf.channel == channel_id);
+
+    let config = match config {
+        Some(conf) => conf,
+        None => return None,
+    };
+
+    let roles = config.get_roles_for_react(&emoji);
+
+    roles
+}
---
+++ src/main.rs
@@ -0,0 +1,148 @@
+use std::{collections::HashMap, rc::Rc, sync::Arc};
+
+use config::{get_react_roles, Config, VecOrSingle};
+use futures::{Future, StreamExt};
+use twilight_gateway::{cluster::ShardScheme, Cluster, Event, EventType, Intents};
+use twilight_http::request::channel::reaction::RequestReactionType;
+use twilight_model::{
+    channel::{Channel, Message, ReactionType},
+    id::{
+        marker::{ChannelMarker, EmojiMarker, MessageMarker},
+        ChannelId, Id,
+    },
+};
+
+mod config;
+mod http;
+
+pub async fn run_socket_event_cluster<
+    H: 'static + Fn(Event) -> F,
+    F: 'static + Future<Output = ()>,
+>(
+    token: String,
+    mut handler: H,
+) {
+    let intents = Intents::GUILDS | Intents::GUILD_MESSAGE_REACTIONS;
+
+    let scheme = ShardScheme::Auto;
+
+    let (cluster, mut events) = Cluster::builder(token, intents)
+        .shard_scheme(scheme)
+        .build()
+        .await
+        .expect("Failed to create cluster");
+
+    tokio::spawn(async move {
+        cluster.up().await;
+    });
+
+    while let Some((id, event)) = events.next().await {
+        let epic = match event.kind() {
+            EventType::ShardConnected
+            | EventType::ShardConnecting
+            | EventType::ShardDisconnected
+            | EventType::ShardIdentifying
+            | EventType::ShardReconnecting
+            | EventType::ShardResuming => true,
+            _ => false,
+        };
+
+        if epic {
+            println!("Shard: {}, Event: {:?}", id, event.kind());
+        }
+
+        handler(event).await;
+    }
+}
+
+fn as_request_reaction_type<'a>(react: &'a ReactionType) -> RequestReactionType<'a> {
+    match react {
+        ReactionType::Custom { name, id, .. } => RequestReactionType::Custom {
+            name: name.as_deref(),
+            id: *id,
+        },
+        ReactionType::Unicode { name } => RequestReactionType::Unicode { name: &name },
+    }
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    dotenv::dotenv().ok();
+
+    let configs = config::read_config();
+
+    let token = std::env::var("TOKEN").expect("TOKEN not set");
+
+    let http = http::HttpApi::new(token.clone());
+
+    // Initialize the reactions
+    for config in configs.iter() {
+        let message = http.get_message(config.channel, config.message).await;
+
+        let channel = http.get_channel(config.channel).await;
+        let channel = match channel {
+            Channel::Guild(guild) => guild,
+            _ => panic!("Channel is not a guild"),
+        };
+
+        let guild = http.get_guild(channel.guild_id().unwrap()).await;
+
+        // Add missing reactions
+        for (emoji_name, _) in config.react_map.iter() {
+            let emoji = http.get_emoji(&guild, emoji_name).await;
+            let my_react = message.reactions.iter().find(|e| e.emoji == emoji && e.me);
+            if my_react.is_none() {
+                http.add_reaction(
+                    config.channel,
+                    config.message,
+                    as_request_reaction_type(&emoji),
+                )
+                .await;
+            }
+        }
+    }
+
+    let http = Rc::new(http);
+    let configs = Rc::new(configs);
+
+    run_socket_event_cluster(token, move |e| {
+        let configs = configs.clone();
+        let http = http.clone();
+
+        async move {
+            match e {
+                Event::ReactionAdd(react) => {
+                    let roles =
+                        get_react_roles(&configs, react.message_id, react.channel_id, &react.emoji);
+
+                    let roles = match roles {
+                        Some(roles) => roles,
+                        None => return,
+                    };
+
+                    for role in roles.iter() {
+                        http.add_role(react.guild_id.unwrap(), react.user_id, role)
+                            .await;
+                    }
+                }
+                Event::ReactionRemove(react) => {
+                    let roles =
+                        get_react_roles(&configs, react.message_id, react.channel_id, &react.emoji);
+
+                    let roles = match roles {
+                        Some(roles) => roles,
+                        None => return,
+                    };
+
+                    for role in roles.iter() {
+                        http.remove_role(react.guild_id.unwrap(), react.user_id, role)
+                            .await;
+                    }
+                }
+
+                _ => {}
+            }
+        }
+    })
+    .await;
+}
